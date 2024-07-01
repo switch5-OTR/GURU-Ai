2d31378d33b9ccf999aa414652f7ac5506d7f69e
switch5-OTR <167744813+switch5-OTR@users.noreply.github.com>
2024-07-01 22:26:51 +0100

    Update config.js

--- config.js
+++ config.js
@@ -23,9 +23,9 @@ for (let i = 0; i < ownerlist.length; i += 2) {
 
 //global.pairingNumber = "" //put your bot number here
 
-global.mods = ['917605902011', '32460248586']
-global.prems = ['917605902011', '32460248586', '919398758484']
-global.allowed = ['917605902011', '32460248586', '19152999993']
+global.mods = ['2348161211372', '2348161211372']
+global.prems = ['2348161211372', '2348161211372', '2348161211372']
+global.allowed = ['2348161211372', '2348161211372', '2348161211372']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = [
